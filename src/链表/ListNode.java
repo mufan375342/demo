362b43c73c0c3b1e59127017a7481620260d06ac
mufan <mufan@jd.com>
2020-03-18 08:42:12 +0800

    添加getSet

--- src/链表/ListNode.java
+++ src/链表/ListNode.java
@@ -5,8 +5,24 @@ package 链表;
  * @time 2019/8/10
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
 
     public ListNode() {
     }
