ed6765595895c6f24dfe5a84d2ab1c260d1493c1
mufan <mufan@jd.com>
2020-04-14 13:58:05 +0800

    两个数之和

---
+++ src/练习/Num_445_两个数之和.java
@@ -0,0 +1,36 @@
+package 练习;
+
+import 链表.ListNode;
+
+import java.util.Stack;
+
+/**
+ * @author mufan
+ * @date 2020/4/14
+ */
+public class Num_445_两个数之和 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        int temp = 0;
+        ListNode head = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || temp > 0) {
+            int sum = temp;
+            sum += stack1.isEmpty() ? 0 : stack1.pop();
+            sum += stack2.isEmpty() ? 0 : stack2.pop();
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            temp = sum / 10;
+        }
+        return head;
+    }
+}
