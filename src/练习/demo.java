c4d58d7ac88763d441e1ed4c77a5f577ec3430a1
mufan <mufan@jd.com>
2020-04-29 09:02:10 +0800

    开心

---
+++ src/练习/demo.java
@@ -0,0 +1,35 @@
+package 练习;
+
+/**
+ * @author mufan
+ * @date 2020/4/27
+ */
+public class demo {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
