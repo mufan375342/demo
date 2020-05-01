a1fbef5684d5fe425b1a295b557fd60aa78f25d6
mufan <mufan@jd.com>
2020-05-01 22:34:16 +0800

    爬楼梯，记忆华搜索，动态规划，递归

--- src/练习/Num_1143_最长公共子序列.java
+++ src/练习/Num_1143_最长公共子序列.java
@@ -9,17 +9,37 @@ public class Num_1143_最长公共子序列 {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
+        //这里需要注意与不同路径的题目是有区别的，不同路径是先进行填充数组,然后在进行计算
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (text1.charAt(i-1) == text2.charAt(j-1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
         return dp[m][n];
+    }
+
+    public int longestCommonSubsequence3rd(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= m; i++) {
+            int last = 0;
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[j] = last + 1;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+                last = temp;
+            }
+        }
+        return dp[n];
     }
 
 }
---
+++ src/练习/Num_70_爬楼梯.java
@@ -0,0 +1,45 @@
+package 练习;
+
+/**
+ * @author mufan
+ * @date 2020/5/1
+ */
+public class Num_70_爬楼梯 {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public int climbStairs1(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int climbStairs2(int n) {
+        int[] memo = new int[n + 1];
+        return test(0, n, memo);
+    }
+
+    private int test(int i, int n, int[] memo) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        return test(i + 1, n, memo) + test(i + 2, n, memo);
+    }
+}
