de39403677d1acd99c863f7c874c13191d2525eb
mufan <mufan@jd.com>
2020-05-26 14:26:07 +0800

    找重复数

---
+++ src/练习/Num_287_寻找重复数.java
@@ -0,0 +1,28 @@
+package 练习;
+
+/**
+ * @author mufan
+ * @date 2020/5/26
+ */
+public class Num_287_寻找重复数 {
+
+    public int findDuplicate(int[] nums) {
+        int left = 1;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+            if (count > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+}
