3bf5619cfa3daceb810c4395f367b805ee8c2a8c
mufan <mufan@jd.com>
2020-06-01 11:18:20 +0800

    解数独

---
+++ src/练习/Num_37_解数独.java
@@ -0,0 +1,52 @@
+package 练习;
+
+/**
+ * @author mufan
+ * @date 2020/5/5
+ */
+public class Num_37_解数独 {
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col] != '.') {
+                    continue;
+                }
+                for (char k = '0'; k < '9'; k++) {
+                    if (!isValid(board, row, col, k)) {
+                        continue;
+                    }
+                    board[row][col] = k;
+                    if (solve(board)) {
+                        return true;
+                    }
+                    board[row][col] = '.';
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char k) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] != '.' && board[row][i] == k) {
+                return false;
+            }
+            if (board[i][col] != '.' && board[i][col] == k) {
+                return false;
+            }
+            char c = board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3];
+            if (c != '.' && c == k) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
