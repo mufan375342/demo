9e221d463979fcc065591c817c2fd469783050fc
mufan <mufan@jd.com>
2020-06-09 15:02:13 +0800

    面试题46

--- src/练习/面试题22_链表中倒数第k个节点.java
+++ src/练习/面试题22_链表中倒数第k个节点.java
@@ -12,6 +12,7 @@ public class 面试题22_链表中倒数第k个节点 {
         ListNode slow = head;
         ListNode fast = head;
         int i = 0;
+        //快指针先走,当快指针走到k的时候慢指针再走,最终的结果在慢指针上
         while (fast != null) {
             if (i >= k) {
                 slow = slow.next;
