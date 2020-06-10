c379c21c04de97bcae62c2ed5a76c9c314c4c256
mufan <mufan@jd.com>
2020-06-10 22:42:18 +0800

    面试题46

---
+++ src/练习/Num_9_回文数.java
@@ -0,0 +1,20 @@
+package 练习;
+
+/**
+ * @author mufan
+ * @date 2020/6/10
+ */
+public class Num_9_回文数 {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int temp = 0;
+        while (x > temp) {
+            temp = temp * 10 + x % 10;
+            x /= 10;
+        }
+        return x == temp || x == temp / 10;
+    }
+}
