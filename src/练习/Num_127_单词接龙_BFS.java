3a2670a17193f82d59fd1c0cb56b587283df0e76
mufan <mufan@jd.com>
2020-06-17 11:00:48 +0800

    单词接龙

--- src/练习/Num_127_单词接龙_BFS.java
+++ src/练习/Num_127_单词接龙_BFS.java
@@ -10,7 +10,9 @@ import java.util.*;
  * @date 2020/4/5
  */
 public class Num_127_单词接龙_BFS {
-
+    /**
+     * 单向BFS
+     */
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
         if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
@@ -59,4 +61,62 @@ public class Num_127_单词接龙_BFS {
         return 0;
     }
 
+    /**
+     * 双向BFS
+     */
+    public int ladderLength1(String beginWord, String endWord, List<String> wordList) {
+        if (wordList.size() == 0 || !wordList.contains(endWord)) {
+            return 0;
+        }
+        Set<String> wordSet = new HashSet<>(wordList);
+
+        Set<String> beginWordVisited = new HashSet<>();
+        beginWordVisited.add(beginWord);
+
+        Set<String> endWordVisited = new HashSet<>();
+        endWordVisited.add(endWord);
+
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        visited.add(endWord);
+
+        int length = beginWord.length();
+
+        int step = 1;
+
+        while (!beginWordVisited.isEmpty() && !endWordVisited.isEmpty()) {
+            if (beginWordVisited.size() > endWordVisited.size()) {
+                Set<String> temp = beginWordVisited;
+                beginWordVisited = endWordVisited;
+                endWordVisited = temp;
+            }
+            Set<String> nextLevelVisited = new HashSet<>();
+            for (String word : beginWordVisited) {
+                char[] chars = word.toCharArray();
+                for (int i = 0; i < length; i++) {
+                    char org = chars[i];
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        if (chars[i] == j) {
+                            continue;
+                        }
+                        chars[i] = j;
+                        String newWord = String.valueOf(chars);
+                        if (wordSet.contains(newWord)) {
+                            if (endWordVisited.contains(newWord)) {
+                                return step + 1;
+                            }
+                            if (!visited.contains(newWord)) {
+                                nextLevelVisited.add(newWord);
+                                visited.add(newWord);
+                            }
+                        }
+                    }
+                    chars[i] = org;
+                }
+            }
+            beginWordVisited = nextLevelVisited;
+            step++;
+        }
+        return 0;
+    }
 }
