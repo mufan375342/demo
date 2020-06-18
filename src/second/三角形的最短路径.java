e54b344660c0f2c0f2ad6df482b4a0ebec966cdf
mufan <mufan@jd.com>
2020-06-18 09:19:09 +0800

    练习

---
+++ src/second/三角形的最短路径.java
@@ -0,0 +1,30 @@
+package second;
+
+/**
+ * @author mufan
+ * @date 2020/6/17
+ */
+public class 三角形的最短路径 {
+
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < col; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+}
