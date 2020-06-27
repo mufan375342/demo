335add4818f224dd575b77e8bfe8126b0eacb810
mufan <mufan@jd.com>
2020-06-27 21:46:26 +0800

    最长回文子串

--- src/练习/Num_5_最长回文子串.java
+++ src/练习/Num_5_最长回文子串.java
@@ -66,35 +66,31 @@ public class Num_5_最长回文子串 {
     /**
      * 中心扩散
      */
+    private int lo;
+    private int maxLen;
+
     public String longestPalindrome3(String s) {
         if (s.length() < 2) {
             return s;
         }
-        String res = s.substring(0, 1);
         for (int i = 0; i < s.length(); i++) {
-            String oddStr = centerSpread(s, i, i);
-            String evenStr = centerSpread(s, i, i + 1);
-            String s1 = oddStr.length() > evenStr.length() ? oddStr : evenStr;
-            if (s1.length() > res.length()) {
-                res = s1;
-            }
+            //odd
+            centerSpread(s, i, i);
+            //even
+            centerSpread(s, i, i + 1);
         }
-        return res;
+        return s.substring(lo, lo + maxLen);
     }
 
-    private String centerSpread(String s, int left, int right) {
+    private void centerSpread(String s, int j, int k) {
         int length = s.length();
-        int i = left;
-        int j = right;
-        while (i >= 0 && j < length) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i--;
-                j++;
-            } else {
-                break;
-            }
+        while (j >= 0 && k < length && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
         }
-        // 这里要小心，跳出 while 循环时，恰好满足 s.charAt(i) != s.charAt(j)，因此不能取 i，不能取 j
-        return s.substring(i + 1, j);
+        if (k - j - 1 > maxLen) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
     }
 }
