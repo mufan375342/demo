798c98736b2b5981ba9818d6a5f43a4d4cfbac5b
mufan <mufan@jd.com>
2020-07-01 09:01:24 +0800

    两个栈实现队列

---
+++ src/练习/Num_718_最长重复子数组.java
@@ -0,0 +1,23 @@
+package 练习;
+
+/**
+ * @author mufan
+ * @date 2020/7/1
+ */
+public class Num_718_最长重复子数组 {
+    public int findLength(int[] A, int[] B) {
+        int res = 0;
+        int[][] dp = new int[A.length + 1][B.length + 1];
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 1; j <= B.length; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+        return res;
+    }
+}
