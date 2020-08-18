4e6a6b2ea0920e0482c52f6c83ae49e8b6f2f8f0
mufan <mufan@jd.com>
2020-08-18 09:39:30 +0800

    平衡二叉树

--- src/每日一题/Num_110_平衡二叉树.java
+++ src/每日一题/Num_110_平衡二叉树.java
@@ -5,7 +5,9 @@ package 每日一题;
  * @date 2020/8/18
  */
 public class Num_110_平衡二叉树 {
-
+    /**
+     * 从底至顶
+     */
     public boolean isBalanced(TreeNode root) {
         return recur(root) != -1;
     }
@@ -23,5 +25,19 @@ public class Num_110_平衡二叉树 {
             return -1;
         }
         return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+
+    /**
+     * 从顶至底
+     */
+    public boolean isBalanced1(TreeNode root) {
+        return Math.abs(depth(root.left) - depth(root.right)) < 2 && isBalanced1(root.left) && isBalanced1(root.right);
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(depth(root.left), depth(root.right)) + 1;
     }
 }
