1979c50262bdb997413aed0a4055b0479c087d8b
mufan <mufan.jd.com>
2020-10-21 00:39:02 +0800

    两个链表第一个公共的交点

--- src/穆帆/链表/两个链表第一个公共的交点.java
+++ src/穆帆/链表/两个链表第一个公共的交点.java
@@ -10,6 +10,7 @@ public class 两个链表第一个公共的交点 {
     /**
      * 两个链表长度分别为L1+C、L2+C， C为公共部分的长度
      * 第一个人走了L1+C步后，回到第二个人起点走L2步；第2个人走了L2+C步后，回到第一个人起点走L1步。 当两个人走的步数都为L1+L2+C时就两个家伙就相爱了
+     *
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode l1 = headA;
